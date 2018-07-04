d335a4f04b170e17e32259e841beefa42f017ef9
Pedro Ivo Aragão Guimarães <pedrodsk@gmail.com>
2018-07-04 01:05:49 -0300

    primeiro commit

---
+++ exemplo0/build/bootloader/bootloader_support/src/esp_image_format.d
@@ -0,0 +1,66 @@
+src/esp_image_format.o: \
+ /home/pedro/esp/esp-idf/components/bootloader_support/src/esp_image_format.c \
+ /home/pedro/esp/esp-idf/components/esp32/include/rom/rtc.h \
+ /home/pedro/esp/esp-idf/components/esp32/include/rom/ets_sys.h \
+ /home/pedro/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /home/pedro/esp/esp-idf/components/esp32/include/esp_assert.h \
+ /home/pedro/esp/esp-idf/components/soc/esp32/include/soc/cpu.h \
+ /home/pedro/esp/esp-idf/components/esp32/include/xtensa/corebits.h \
+ /home/pedro/esp/esp-idf/components/bootloader_support/include/esp_image_format.h \
+ /home/pedro/esp/esp-idf/components/esp32/include/esp_err.h \
+ /home/pedro/esp/esp-idf/components/bootloader_support/include/esp_flash_partitions.h \
+ /home/pedro/esp/esp-idf/components/esp32/include/esp_flash_data_types.h \
+ /home/pedro/esp/exemplo3/build/bootloader/../include/sdkconfig.h \
+ /home/pedro/esp/esp-idf/components/bootloader_support/include/esp_secure_boot.h \
+ /home/pedro/esp/esp-idf/components/soc/esp32/include/soc/efuse_reg.h \
+ /home/pedro/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /home/pedro/esp/esp-idf/components/log/include/esp_log.h \
+ /home/pedro/esp/esp-idf/components/esp32/include/rom/ets_sys.h \
+ /home/pedro/esp/esp-idf/components/log/include/esp_log_internal.h \
+ /home/pedro/esp/esp-idf/components/spi_flash/include/esp_spi_flash.h \
+ /home/pedro/esp/esp-idf/components/bootloader_support/include_priv/bootloader_flash.h \
+ /home/pedro/esp/esp-idf/components/bootloader_support/include_priv/bootloader_random.h \
+ /home/pedro/esp/esp-idf/components/bootloader_support/include_priv/bootloader_sha.h
+
+/home/pedro/esp/esp-idf/components/esp32/include/rom/rtc.h:
+
+/home/pedro/esp/esp-idf/components/esp32/include/rom/ets_sys.h:
+
+/home/pedro/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+
+/home/pedro/esp/esp-idf/components/esp32/include/esp_assert.h:
+
+/home/pedro/esp/esp-idf/components/soc/esp32/include/soc/cpu.h:
+
+/home/pedro/esp/esp-idf/components/esp32/include/xtensa/corebits.h:
+
+/home/pedro/esp/esp-idf/components/bootloader_support/include/esp_image_format.h:
+
+/home/pedro/esp/esp-idf/components/esp32/include/esp_err.h:
+
+/home/pedro/esp/esp-idf/components/bootloader_support/include/esp_flash_partitions.h:
+
+/home/pedro/esp/esp-idf/components/esp32/include/esp_flash_data_types.h:
+
+/home/pedro/esp/exemplo3/build/bootloader/../include/sdkconfig.h:
+
+/home/pedro/esp/esp-idf/components/bootloader_support/include/esp_secure_boot.h:
+
+/home/pedro/esp/esp-idf/components/soc/esp32/include/soc/efuse_reg.h:
+
+/home/pedro/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+
+/home/pedro/esp/esp-idf/components/log/include/esp_log.h:
+
+/home/pedro/esp/esp-idf/components/esp32/include/rom/ets_sys.h:
+
+/home/pedro/esp/esp-idf/components/log/include/esp_log_internal.h:
+
+/home/pedro/esp/esp-idf/components/spi_flash/include/esp_spi_flash.h:
+
+/home/pedro/esp/esp-idf/components/bootloader_support/include_priv/bootloader_flash.h:
+
+/home/pedro/esp/esp-idf/components/bootloader_support/include_priv/bootloader_random.h:
+
+/home/pedro/esp/esp-idf/components/bootloader_support/include_priv/bootloader_sha.h:
+/home/pedro/esp/esp-idf/components/bootloader_support/src/esp_image_format.c:
