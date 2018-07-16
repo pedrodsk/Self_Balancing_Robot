6b1ea3a08d9fa9663538389527a9307dc1757209
Pedro Ivo Aragão Guimarães <pedro.guimaraes@ee.ufcg.edu.br>
2018-07-16 16:16:00 -0300

    deletando pasta robopendulo

--- roboPendulo/build/MPUdriver/component_project_vars.mk
+++
@@ -1,7 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(IDF_PATH)/components/MPUdriver/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/MPUdriver -lMPUdriver
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += MPUdriver
-component-MPUdriver-build: 
--- roboPendulo/build/main/kalmanfilter.d
+++
@@ -1,5 +0,0 @@
-kalmanfilter.o: /home/pedro/esp/RoboPendulo/main/kalmanfilter.cpp \
- /home/pedro/esp/RoboPendulo/main/include/kalmanfilter.hpp
-
-/home/pedro/esp/RoboPendulo/main/include/kalmanfilter.hpp:
-/home/pedro/esp/RoboPendulo/main/./kalmanfilter.cpp:
